74c40f96e8c2eb1fbe964855de54a0fa84b25fe3
Dereck Mezquita <dereck@mezquita.io>
2024-09-06 15:42:52 -0500

    STAT-11: Better docs.

--- src/descriptive/mean.ts
+++ src/descriptive/mean.ts
@@ -3,6 +3,9 @@ import { sum } from '@ts-statscript/base';
 /**
  * Calculates the arithmetic mean (average) of an array of numbers.
  *
+ * This function uses the optimised `sum` function from
+ * `@ts-statscript/base` to compute the sum of the input array.
+ *
  * @param x - The input array of numbers.
  * @returns The mean value of the array, or NaN if the array is empty.
  */
