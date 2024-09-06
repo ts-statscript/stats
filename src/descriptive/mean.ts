8afd9e09d619e984d248583e1d6e216d2943e52a
Dereck Mezquita <dereck@mezquita.io>
2024-09-06 15:29:55 -0500

    STAT-11: Mean function uses base unrolled sum function.

--- src/descriptive/mean.ts
+++ src/descriptive/mean.ts
@@ -1,5 +1,4 @@
 import { sum } from '@ts-statscript/base';
-import { variance } from './variance';
 
 /**
  * Calculates the arithmetic mean (average) of an array of numbers.
@@ -9,21 +8,6 @@ import { variance } from './variance';
  */
 export function mean(x: number[]): number {
     if (x.length === 0) return NaN;
-    // const sum = x.reduce((acc, val) => acc + val, 0);
     const sum_res = sum(x);
     return sum_res / x.length;
-}
-
-/**
- * Calculates the standard deviation of an array of numbers.
- *
- * The standard deviation is the square root of the variance.
- *
- * @param x - The input array of numbers.
- * @returns The standard deviation of the array, or NaN if the array has fewer than 2 elements.
- */
-export function standardDeviation(x: number[]): number {
-    if (x.length < 2) return NaN;
-    const varianceValue = variance(x);
-    return Math.sqrt(varianceValue);
 }
